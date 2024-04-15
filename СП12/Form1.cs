d6a7699877b9acf445a8ecad03899e94dec2f849
Coffey <H1.coffey@bk.ru>
2024-04-16 01:53:40 +0700

    12

---
+++ СП12/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace СП12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InputLanguageCollection installedLangs = InputLanguage.InstalledInputLanguages;
+            foreach (InputLanguage lang in installedLangs)
+            {
+                dataGridView1.Rows.Add(lang.Culture.Name, lang.LayoutName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentCell != null)
+            {
+                int n = dataGridView1.CurrentCell.RowIndex;
+                string languageName = dataGridView1.Rows[n].Cells[0].Value.ToString();
+                InputLanguage lang = InputLanguage.FromCulture(new CultureInfo(languageName));
+                InputLanguage.CurrentInputLanguage = lang;
+                MessageBox.Show($"Установлен язык: {languageName}", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Выберите язык для установки.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
